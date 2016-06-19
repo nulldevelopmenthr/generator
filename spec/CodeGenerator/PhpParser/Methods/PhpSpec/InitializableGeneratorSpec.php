5d3e719952426bae3a40290f283548d83c1d840d
Miro Svrtan <miro@mirosvrtan.me>
2016-06-19 14:42:25 +0200

    New methods needed for PhpSpec

---
+++ spec/CodeGenerator/PhpParser/Methods/PhpSpec/InitializableGeneratorSpec.php
@@ -0,0 +1,21 @@
+<?php
+
+declare (strict_types = 1);
+namespace spec\NullDev\Skeleton\CodeGenerator\PhpParser\Methods\PhpSpec;
+
+use PhpParser\BuilderFactory;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class InitializableGeneratorSpec extends ObjectBehavior
+{
+    public function let(BuilderFactory $builderFactory)
+    {
+        $this->beConstructedWith($builderFactory);
+    }
+
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType('NullDev\Skeleton\CodeGenerator\PhpParser\Methods\PhpSpec\InitializableGenerator');
+    }
+}
---
+++ src/CodeGenerator/PhpParser/Methods/PhpSpec/InitializableGenerator.php
@@ -0,0 +1,47 @@
+<?php
+
+declare (strict_types = 1);
+namespace NullDev\Skeleton\CodeGenerator\PhpParser\Methods\PhpSpec;
+
+use NullDev\Skeleton\Definition\PHP\Methods\PhpSpec\InitializableMethod;
+use PhpParser\BuilderFactory;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\ConstFetch;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Name;
+use PhpParser\Node\Scalar\String_;
+
+class InitializableGenerator
+{
+    private $builderFactory;
+
+    public function __construct(BuilderFactory $builderFactory)
+    {
+        $this->builderFactory = $builderFactory;
+    }
+
+    public function generate(InitializableMethod $method)
+    {
+        $node = $this->builderFactory
+            ->method($method->getMethodName())
+            ->makePublic();
+
+        foreach ($method->getShouldHaveTypes() as $classType) {
+            $node->addStmt(
+                new MethodCall(
+                    new Variable('this'),
+                    'shouldHaveType',
+                    [
+                        new ClassConstFetch(
+                            new Name($classType->getName()),
+                            'class'
+                        ),
+                    ]
+                )
+            );
+        }
+
+        return $node;
+    }
+}
